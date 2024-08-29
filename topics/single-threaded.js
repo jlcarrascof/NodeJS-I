d326bbd64aba7797631b2914624590c6788b229d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 18:21:51 -0400

    chore: Refactor single-threaded.js to use a counter in the setInterval function

--- topics/single-threaded.js
+++ topics/single-threaded.js
@@ -1,5 +1,8 @@
 console.log('Hello world');
 
+let i = 0;
 setInterval(function() {
-    console.log("I'm still alive");
+    console.log(i);
+    i++;
+    // console.log("I'm still alive");
 }, 1000);
