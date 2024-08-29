824d5f4a2924a28ff87347fdf3dd64e285d07e06
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 18:25:18 -0400

    chore: Fix bug in single-threaded.js causing process abortion

--- topics/single-threaded.js
+++ topics/single-threaded.js
@@ -5,4 +5,8 @@ setInterval(function() {
     console.log(i);
     i++;
     // console.log("I'm still alive");
+    if (i === 5) {
+        console.log('Forcefully aborting the process');
+        var a = 3 + z;
+    }
 }, 1000);
