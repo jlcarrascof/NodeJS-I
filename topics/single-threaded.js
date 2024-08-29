46cbe3e13bafe261f1e2a910e579e0155a8c175c
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-29 18:30:28 -0400

    chore: Remove commented code in single-threaded.js

--- topics/single-threaded.js
+++ topics/single-threaded.js
@@ -5,10 +5,10 @@ setInterval(function() {
     console.log(i);
     i++;
     // console.log("I'm still alive");
-    if (i === 5) {
-        console.log('Forcefully aborting the process');
-        var a = 3 + z;
-    }
+    // if (i === 5) {
+    //     console.log('Forcefully aborting the process');
+    //     var a = 3 + z;
+    // }
 }, 1000);
 
 console.log('This is the end of the script');
