055b6037acc18c0588268cb5eb533cda1975c16c
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-04 19:59:36 -0400

    Add promises.js with hello, talk, and goodbye functions for asynchronous handling

---
+++ async/promises.js
@@ -0,0 +1,21 @@
+function hello(name, myCallback) {
+    setTimeout(function() {
+        console.log("Hi, " + name)
+        myCallback(name)
+    }, 1500)
+}
+
+function talk(callbackTalk) {
+    setTimeout(function() {
+        console.log("Bla bla bla...")
+        callbackTalk()
+    }
+    , 1000)
+}
+
+function goodbye(name, secondCallback) {
+    setTimeout(function() {
+        console.log("Goodbye, " + name)
+        secondCallback()
+    }, 1000)
+}
