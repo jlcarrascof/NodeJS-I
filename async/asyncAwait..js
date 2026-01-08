ab8adba79449cde5339849c99c4813454fc610c9
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-01-08 18:52:20 -0400

    Add async functions for greeting, talking, and farewell processes

---
+++ async/asyncAwait..js
@@ -0,0 +1,31 @@
+function hello(name) {
+
+    return new Promise(function (resolve, reject) {
+        setTimeout(function() {
+            console.log("Hi, " + name)
+            resolve(name)
+        }, 1500)
+    })
+}
+
+function talk(name) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log("Bla bla bla...")
+            // resolve(name)
+            reject('There is an error')
+        }
+        , 1000)
+    })
+
+}
+
+function goodbye(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log("Goodbye, " + name)
+            resolve()
+        }, 1000)
+    })
+}
